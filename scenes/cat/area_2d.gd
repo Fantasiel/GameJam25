f81efd182cf1e2f09e99b742b075efb4b40d825e
stefangugger <stegu_@hotmail.de>
2025-08-03 14:14:12 +0200

    add collision objects

--- scenes/cat/area_2d.gd
+++ scenes/cat/area_2d.gd
@@ -15,7 +15,6 @@ func _on_body_entered(body: Object) -> void:
 	var cat = get_parent()
 	if body is Vase and not body.is_broken:
 		body.show_slappability_hint.emit(true)
-		cat.slappable_bodies[body.get_instance_id()] = body;
 		
 
 func _on_body_exited(body) -> void:
@@ -27,4 +26,9 @@ func _on_body_exited(body) -> void:
 
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if PhysicsServer2D.body_get_collision_layer(body_rid) & 8 > 0:
+		died.emit()
+
+
+func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	if PhysicsServer2D.area_get_collision_layer(area_rid) & 8 > 0:
 		died.emit()
