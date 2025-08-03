9233183b78511a4d81a7a09e472683528266700e
Tom Morris Feist <morris@feist.eu>
2025-08-03 16:00:43 +0200

    Add levels

---
+++ scenes/levels/level6/level_6.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+signal victory 
+
+func _ready() -> void:
+	self.connect("victory", Callable(self, "_on_victory"))
+	
+func _on_victory() -> void: 
+	await get_tree().create_timer(2.0).timeout
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
