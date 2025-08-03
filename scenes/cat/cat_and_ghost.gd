b2ce0d3f98e4a31b43301b57be8253e9fc512847
stefangugger <stegu_@hotmail.de>
2025-08-03 04:08:21 +0200

    remove old unused files

--- scenes/cat/cat_and_ghost.gd
+++ scenes/cat/cat_and_ghost.gd
@@ -3,16 +3,16 @@ extends Node2D
 const DISTANCE_TO_ENABLE_INTERACTION = 32*32
 
 func replay() -> void:
+	$Ghost.recording_data = $Cat.recording_data
+	$Cat.recording_data = {}
+
+func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("replay"):
-		$Ghost.recording_data = $Cat.recording_data
-		$Cat.recording_data = {}
+		replay()
 	
 	if $Cat.position.distance_squared_to($Ghost.position) >= DISTANCE_TO_ENABLE_INTERACTION:
 		$Cat.set_collision_layer_value(2, true)
 		$Ghost.set_collision_layer_value(2, true)
-
-func _physics_process(delta: float) -> void:
-	replay()
 
 
 func _on_cat_on_replay() -> void:
